5d5d042b817b117ac155ad334b897b06235ad3a6
Antoine Blancke <antoine.blancke@gmail.com>
2016-02-23 19:39:44 +0100

    android tutorial step 2

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Angular2/Typescript Tutorial
 
-Notes : 
+Notes :
+
+The Typescript compiler can be launched in watch mode : "tsc -w"
+
+Transpiling a class to JavaScript produces code. Transpiling an interface produces — nothing. If the class does nothing, we prefer an interface.
+
+ngModel built-in directive for two-way binding
--- app/app.components.ts
+++ app/app.components.ts
@@ -1,7 +1,26 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App : Hero Editor</h1>'
+    selector: "my-app",
+    template: `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+  </div>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+  public title = "Tour of heroes";
+  public hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  };
+}
+
+interface Hero {
+  id: number;
+  name: string;
+}
